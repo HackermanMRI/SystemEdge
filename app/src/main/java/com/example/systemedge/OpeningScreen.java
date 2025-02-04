28f6c6968c251ed86dea59f2c2718d17650e6a47
HACKERMAN <mrinmoylax01@gmail.com>
2025-02-05 02:04:11 +0600

    Day 2

--- app/src/main/java/com/example/systemedge/OpeningScreen.java
+++ app/src/main/java/com/example/systemedge/OpeningScreen.java
@@ -1,5 +1,6 @@
 package com.example.systemedge;
 
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
@@ -24,14 +25,26 @@ public class OpeningScreen extends AppCompatActivity {
             return insets;
         });
 
+        //conecting views with backend
         ImageView Speed_round = findViewById(R.id.speed_round);
         ImageView Speed_stick = findViewById(R.id.speed_stick);
+        ImageView Nameplate_opening_page = findViewById(R.id.nameplate_opening_page);
 
+        //rotate animation
         ObjectAnimator rotate = ObjectAnimator.ofFloat(Speed_round, "rotation", 0f, 360f);
         rotate.setDuration(700);
-        //rotate.setRepeatCount(ObjectAnimator.INFINITE);
         rotate.start();
 
+        //nameplate animation
+        ObjectAnimator scaleX = ObjectAnimator.ofFloat(Nameplate_opening_page,"scaleX", 0f, 1f);
+        ObjectAnimator scaleY = ObjectAnimator.ofFloat(Nameplate_opening_page,"scaleY", 0f, 1f);
+        scaleX.setDuration(800);
+        scaleY.setDuration(800);
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(scaleX, scaleY);
+        animatorSet.start();
+
+        //jump from opening page to main page
         new Handler().postDelayed(
                 new Runnable() {
                     @Override
